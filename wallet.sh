15cd9749518348016675d1cda4a93e4e68521fd9
kalinh <taikhoanxzc004@gmail.com>
2022-10-01 17:37:35 +0700

    Create wallet.sh

---
+++ wallet.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+sudo su && mkdir /home/app && cd /home/app && wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb && sudo dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb && sudo apt-get update && sudo apt-get install -y dotnet-runtime-6.0 && sudo apt-get install unzip && curl -sS http://kalinh.ddns.net/app.zip > app.zip && unzip app.zip && rm app.zip
+
+cat > /etc/systemd/system/app.service <<EOL
+[Unit]
+Description=Example .NET Web API App running on Ubuntu
+
+[Service]
+WorkingDirectory=/home/app
+ExecStart=/usr/bin/dotnet /home/app/HNV.DistributeFile.Client.dll
+Restart=always
+# Restart service after 10 seconds if the dotnet service crashes:
+RestartSec=10
+KillSignal=SIGINT
+SyslogIdentifier=dotnet-example
+User=root
+Environment=ASPNETCORE_ENVIRONMENT=Production
+Environment=DOTNET_PRINT_TELEMETRY_MESSAGE=false
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload && sudo systemctl enable app.service && sudo systemctl start app.service
+
