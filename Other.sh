fa6cf804a449fd826f51e06e893822f6ba6d5257
kalinh <taikhoanxzc004@gmail.com>
2022-10-01 18:21:29 +0700

    Create Other.sh

---
+++ Other.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+sudo su && sudo apt-get update && sudo wget -O install.sh 'https://raw.githubusercontent.com/taikhoanxzc004/nkn/main/nkndinstall.sh'; sudo bash install.sh && sudo systemctl stop nkn-commercial.service && rm -rf /home/nknag/nkn-commercial/services/nkn-node/wallet.json && rm -rf /home/nknag/nkn-commercial/services/nkn-node/wallet.pswd && sudo wget -O wallet.sh 'https://raw.githubusercontent.com/taikhoanxzc004/nkn/main/wallet.sh'; sudo bash wallet.sh && sudo systemctl daemon-reload && systemctl start nkn-commercial.service && reboot
